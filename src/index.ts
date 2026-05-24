2d021fc9d602cafbcfbda5b8f832a4378c0b895b
Augusto Gabriel de Almeida Santos <augusto.031109@gmail.com>
2026-05-24 06:15:45 +0000

    feat: enable CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import Fastify from "fastify";
 import detect from "./routes/detect";
+import cors from "@fastify/cors";
 
 const app = Fastify();
 
 app.register(detect);
-
+app.register(cors, {
+    origin: "*",
+    methods: ["GET"]
+});
 const PORT = Number(process.env.PORT);
 
 app.get("/", async (request, reply) => {
